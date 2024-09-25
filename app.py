0f7a0bdb25a5bbf2c99032b23bb3da1e30e0216e
shrin <test@email.com>
2024-09-25 09:07:34 +0530

    Change port

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def weather():
     return render_template('index.html', data=weather_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
