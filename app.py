8a0a19106eb135c1ae234b09ce628066408f3030
shrin <test@email.com>
2024-09-25 08:33:15 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import json
+import urllib.request
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+
+app=Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def weather():
+    if request.method == 'POST':
+        city = request.form['city']
+    else:
+        city = 'London'
+
+    source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key).read()
+
+    data=json.loads(source)
+    print(data)
+    weather_data = {"country_code": data['sys']['country'],"name":data['sys']['name'], "coordinate": data['coord']['lon'], "coordinate": data['coord']['lat'], "temp": data['main']['temp'], "pressure": data['main']['pressure'], "humidity": data['main']['humidity'], "weather": data['weather'][0]['description'], "icon": data['weather'][0]['icon']}
+    return render_template('index.html', data=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
